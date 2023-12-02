c854f65df898160e73aac7e801e0f9a9990d495e
NingWang0123 <1198137403wn@gmail.com>
2023-12-02 17:18:23 -0500

    update the helpers and report
    add the data visualization for the reprt. Move functions into helpers py file. Fix the error in data cleaning process

--- helpers.py
+++ helpers.py
@@ -1,5 +1,31 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 def filter_out_cate(df,column):
-    if len(filtered_df[column].unique())>10:
+    if len(df[column].unique())>10:
         print('column needs to be removed')
     else:
         print('column does not need to be removed')
+        
+        
+def visualize_data(df):
+    sns.set(style="whitegrid")
+    scatter_columns = ['District', 'Stories', 'Year_Built', 'Units', 'Bdrms', 'Fbath', 'Hbath', 'Lotsize']
+    for col in scatter_columns:
+        plt.figure(figsize=(10, 6))
+        sns.scatterplot(x=df[col], y=df['Sale_price'])
+        plt.title(f'{col} vs Sale_price')
+        plt.xlabel(col)
+        plt.ylabel('Sale_price')
+        plt.show()
+
+    df['COVID_Status'] = df.apply(lambda x: 'COVID Period' if x['covid_period_yes'] == 1 else 'Non-COVID Period', axis=1)
+    plt.figure(figsize=(10, 6))
+    sns.boxplot(x='COVID_Status', y='Sale_price', data=df)
+    plt.title('Boxplot of Sale_price by COVID Status')
+    plt.xlabel('COVID Status')
+    plt.ylabel('Sale Price')
+    plt.show()
+    
