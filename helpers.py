998f6745cfbb5f3b55752c119883e42bbc7226c4
NingWang0123 <1198137403wn@gmail.com>
2023-12-03 18:32:46 -0500

    update the final report
    update the final report

--- helpers.py
+++ helpers.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD, Adam
+from sklearn.metrics import mean_squared_error
 
 def filter_out_cate(df,column):
     if len(df[column].unique())>10:
@@ -42,16 +43,30 @@ def create_model(layers, activation, solver, alpha, learning_rate, input_dim):
     model.compile(optimizer=solver, loss='mean_squared_error')
     return model
 
+def create_keras_model(layers, activation, solver, alpha, learning_rate, input_dim):
+    model = Sequential()
+    for i, layer_size in enumerate(layers):
+        if i == 0:
+            model.add(Dense(layer_size, activation=activation, input_dim=input_dim))
+        else:
+            model.add(Dense(layer_size, activation=activation))
+    model.add(Dense(1, activation='linear')) 
+    model.compile(optimizer=solver, loss='mean_squared_error')
+    return model
+
+
+
+
 def evaluate_keras_model(model, X, y, cv_splits):
     rmse_scores = []
     for train_idx, test_idx in cv_splits.split(X):
         X_train, X_test = X[train_idx], X[test_idx]
         y_train, y_test = y[train_idx], y[test_idx]
 
-        X_train = np.array(X_train).astype('float32')
-        y_train = np.array(y_train).astype('float32')
-        X_test = np.array(X_test).astype('float32')
-        y_test = np.array(y_test).astype('float32')
+        X_train, X_test, y_train, y_test = map(
+            lambda array: np.array(array).astype('float32'),
+            [X_train, X_test, y_train, y_test]
+        )
 
         if np.any(np.isnan(X_train)) or np.any(np.isinf(X_train)):
             raise ValueError("NaN or Inf in X_train")
@@ -64,9 +79,40 @@ def evaluate_keras_model(model, X, y, cv_splits):
 
         model.fit(X_train, y_train, epochs=1000, batch_size=32, verbose=0)
 
-        predictions = model.predict(X_test)
-        mse = np.mean((predictions.flatten() - y_test) ** 2)
+        predictions = model.predict(X_test).flatten()
+
+        mse = np.mean((predictions - y_test) ** 2)
         rmse_scores.append(np.sqrt(mse))
 
     return np.mean(rmse_scores)
+
+
+def compute_rmse(y_true, y_pred):
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+def fit_keras_model(model,X_train,y_train):
+    X_train, y_train = map(
+            lambda array: np.array(array).astype('float32'),
+            [X_train, y_train]
+        )
+    if np.any(np.isnan(X_train)) or np.any(np.isinf(X_train)):
+        raise ValueError("NaN or Inf in X_train")
+    if np.any(np.isnan(y_train)) or np.any(np.isinf(y_train)):
+        raise ValueError("NaN or Inf in y_train")
+    model.fit(X_train, y_train, epochs=1000, batch_size=32, verbose=0) 
+    return model
+
+def keras_rmse(X_test,Y_test,model):
+    X_test, Y_test = map(
+            lambda array: np.array(array).astype('float32'),
+            [X_test, Y_test]
+        )
+    if np.any(np.isnan(X_test)) or np.any(np.isinf(X_test)):
+        raise ValueError("NaN or Inf in X_train")
+    if np.any(np.isnan(Y_test)) or np.any(np.isinf(Y_test)):
+        raise ValueError("NaN or Inf in y_train")
+    predictions = model.predict(X_test).flatten()
+
+    mse = np.mean((predictions - y_test) ** 2)
+    return np.sqrt(mse)
 
