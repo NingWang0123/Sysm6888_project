9b99db0dee3fcb0721112b843b50023f8454cc04
NingWang0123 <1198137403wn@gmail.com>
2023-12-02 19:00:09 -0500

    fix some bugs

--- helpers.py
+++ helpers.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD, Adam
 
 def filter_out_cate(df,column):
     if len(df[column].unique())>10:
@@ -29,3 +32,30 @@ def visualize_data(df):
     plt.ylabel('Sale Price')
     plt.show()
     
+def create_model(hidden_layer_sizes, activation, solver, alpha, learning_rate, input_dim):
+    model = Sequential()
+    model.add(Dense(hidden_layer_sizes[0], input_dim=input_dim, activation=activation))
+    for units in hidden_layer_sizes[1:]:
+        model.add(Dense(units, activation=activation))
+    model.add(Dense(1))  # Output layer for regression
+
+    if solver == 'sgd':
+        optimizer = SGD(learning_rate=learning_rate)
+    elif solver == 'adam':
+        optimizer = Adam(learning_rate=learning_rate)
+
+    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mse'])
+    return model
+
+def evaluate_keras_model(model, X, y, cv_splits):
+    rmse_scores = []
+    for train_idx, test_idx in cv_splits.split(X):
+        X_train_k, X_test_k = X[train_idx], X[test_idx]
+        y_train_k, y_test_k = y[train_idx], y[test_idx]
+        model.fit(X_train_k, y_train_k, epochs=1000, batch_size=32, verbose=0)
+        predictions = model.predict(X_test_k)
+        mse = np.mean((predictions.flatten() - y_test_k) ** 2)
+        rmse_scores.append(np.sqrt(mse))
+    return np.mean(rmse_scores)
+
+
